e281023ddf1fc239265c17b4323f2b0b8fc944db
HaruyaOno <166776790+22FI024HaruyaOno@users.noreply.github.com>
2024-06-14 11:14:05 +0900

    commit

--- main.py
+++ main.py
@@ -31,3 +31,17 @@ def omikuji():
     ]
 
     return omikuji_list[random.randrange(10)]
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
