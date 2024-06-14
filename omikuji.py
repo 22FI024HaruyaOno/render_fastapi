a325227faa136660a7eb3d278e78c299bc34d626
HaruyaOno <166776790+22FI024HaruyaOno@users.noreply.github.com>
2024-06-14 11:01:59 +0900

    commit

---
+++ omikuji.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+import random  # randomライブラリを追加
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+
+    return omikuji_list[random.randrange(10)]
